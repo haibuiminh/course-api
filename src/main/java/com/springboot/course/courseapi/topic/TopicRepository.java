e3e241cba12127edddbecd20c7093b1620df5f0d
haibuiminh <61405445+haibuiminh@users.noreply.github.com>
2021-03-26 23:57:31 +0700

    Course-setup: connect database

---
+++ src/main/java/com/springboot/course/courseapi/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.course.courseapi.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
